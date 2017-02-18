d13f001037f2e985574c6ced0c8b489a1d643c02
Eden Sun <huntack478@gmail.com>
2017-02-18 14:57:12 +0800

    with test

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-// module.exports.xxx
-const { exec } = require('child_process')
+const { spawn } = require('child_process')
 
-function getInterfaces () {
-  let options = []
-  let allInterfaces = require('os').networkInterfaces()
-  for (let intfName in allInterfaces) {
-    options.push(
-      ...allInterfaces[intfName]
-      .filter(address => address.family === 'IPv4' && !address.internal)
-      .map(m => Object.assign(m, { interface: intfName }))
-    )
-  }
-  return options
+// function getInterfaces () {
+//   let options = []
+//   let allInterfaces = require('os').networkInterfaces()
+//   for (let intfName in allInterfaces) {
+//     options.push(
+//       ...allInterfaces[intfName]
+//       .filter(address => address.family === 'IPv4' && !address.internal)
+//       .map(m => Object.assign(m, { interface: intfName }))
+//     )
+//   }
+//   return options
+// }
+//
+// console.log(getInterfaces())
+
+module.exports.setMac = function (ip, mac) {
+  return new Promise((resolve, reject) => {
+    var arp = spawn("arp", [ "-S", ip, mac ])
+    var buffer = '';
+    var errstream = '';
+    arp.stdout.on('data', function (data) {
+      buffer += data;
+    })
+    arp.stderr.on('data', function (data) {
+      errstream += data;
+    })
+
+    arp.on('close', function (code) {
+      if (code !== 0) reject(errstream)
+      resolve()
+    })
+  })
 }
-
-console.log(getInterfaces())
---
+++ test/index.js
@@ -0,0 +1,18 @@
+require('should')
+var lib = require('../index.js')
+var arp = require('node-arp')
+const testIP = '192.168.16.1'
+const testMac = '5c:3d:33:af:6b:5' + Math.floor(Math.random() * 10)
+// const testMac = '5c:3d:33:af:6b:56'
+console.log('testing Mac = ', testMac)
+describe('api spec test', () => {
+  it('set arp', (done) => {
+    lib.setMac(testIP, testMac).then(done)
+  })
+
+  it('read result', () => {
+    arp.getMAC(testIP, (err, mac) => {
+      mac.should.equal(testMac)
+    })
+  })
+})
