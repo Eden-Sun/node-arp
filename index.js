4dedd1015d99a9c2f2cb1ad40271a40bd1b4af15
Eden Sun <huntack478@gmail.com>
2017-02-18 13:01:31 +0800

    function to get all interfaces

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-modules
+// module.exports.xxx
+const { exec } = require('child_process')
+
+function getInterfaces () {
+  let options = []
+  let allInterfaces = require('os').networkInterfaces()
+  for (let intfName in allInterfaces) {
+    options.push(
+      ...allInterfaces[intfName]
+      .filter(address => address.family === 'IPv4' && !address.internal)
+      .map(m => Object.assign(m, { interface: intfName }))
+    )
+  }
+  return options
+}
+
+console.log(getInterfaces())
